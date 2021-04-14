796f5b12cb86905b2e423ddd95601d0459ccede5
Dwale Dev <superdwale@gmail.com>
2021-04-14 03:43:14 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TimelineMax } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gsap-with-ng';
+   menu = new TimelineMax({paused:true, reversed:true});
+
+  ngOnInit(){
+    this.createMenuAnim()
+  }
+
+  createMenuAnim(){
+    this.menu.to("#topLine", .5, {rotation:'30', ease:"Expo.easeInOut"},0)
+    this.menu.to("#midLine", .5, {opacity:'0', ease:"Expo.easeInOut"},0)
+    this.menu.to("#botLine", .5, {rotation:'-30', ease:"Expo.easeInOut"},0)
+  }
+  
+  menuClick() {
+    this.menu.reversed() ? this.menu.play() : this.menu.reverse();	
+    return console.log('clicked');
+   }
 }
